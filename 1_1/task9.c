fe3230dc29b105e35cf9228a290ae869554968f8
lucifgy <lucifg@gmail.com>
2022-10-27 09:01:08 +0300

    V2:Realloc update prototype:-Not yet working

--- 1_1/task9.c
+++ 1_1/task9.c
@@ -11,69 +11,182 @@
  *  -mem alloc in "my" functions should occur here.
  *  -Outside of init() cannot use malloc, realloc, free functions.
  *
- *
  */
-//TODO:
-//Freed mem merge
-//~
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+#include <memory.h>
 
-#define MAX_SIZE 500
+#define MAX_MEMORY_SIZE (160)
 
 #pragma pack(1)
 
-unsigned char* malloc_data;
-size_t mallocUsed;
-
-void init() 
+typedef struct Meta
 {
-    malloc_data = (unsigned char*)malloc(MAX_SIZE * sizeof(unsigned char));
-    mallocUsed = 0;
+	size_t size;
+	unsigned char allocated;
+	struct Block *next;
+	struct Block *prev;
+}Meta;
+
+typedef struct Block
+{
+	struct Meta meta;
+	char data[];
+}Block;
+
+struct Block *mem_block;
+
+void fillMeta(Meta *meta, size_t size, Block *next, Block *prev, unsigned char allocated);
+
+void merge(Block *block);
+
+void init()
+{
+	mem_block = (Block *) malloc(MAX_MEMORY_SIZE);
+	mem_block->meta.size = MAX_MEMORY_SIZE - sizeof(Meta);
+	mem_block->meta.next = NULL;
+	mem_block->meta.prev = NULL;
+	mem_block->meta.allocated = 0;
 }
 
 void *myMalloc(size_t size) 
 {
-    void* p = &malloc_data[mallocUsed];
-    mallocUsed += size;
-    return p;
+	if (size == 0)
+		return NULL;
+	Block *block = mem_block;
+	while (block) {
+		if (block->meta.allocated) 
+		{   
+			block = block->meta.next;
+			continue;
+		}   
+		size_t needed_space = size;
+		if (block->meta.size < needed_space) 
+		{   
+			block = block->meta.next;
+			continue;
+		}   
+		size_t remaining_space = block->meta.size - needed_space;
+		fillMeta(&block->meta, size, block->meta.next, block->meta.prev, 1); 
+		if (remaining_space > sizeof(Meta))
+		{   
+			Block *remainedBlock = (Block *) block->data + size;
+			fillMeta(&remainedBlock->meta, remaining_space - sizeof(Meta), block->meta.next,    block, 0);
+			block->meta.next = remainedBlock;
+			if(remainedBlock->meta.next)
+			{   
+				Block *next = remainedBlock->meta.next;
+				next->meta.prev = remainedBlock;
+			}   
+		}
+		return block->data;
+	}
+	printf("No space avaible to alloc\n");
+	return NULL;
 }
 
-void myFree(void *ptr) 
+void myFree(void *ptr)
 {
-    ptr = NULL;
+	if (!ptr)
+		return;
+	Block *block = mem_block;
+	while (block)
+	{
+		if (block->data == ptr)
+		{
+			block->meta.allocated = 0;
+			merge(block);
+			return;
+		}
+		block = block->meta.next;
+	}
 }
 
-void *myRealloc(void *ptr, size_t size) 
+void *myRealloc(void *ptr, size_t size)
 {
-    ptr = &malloc_data[mallocUsed];
-    mallocUsed = size;
-	return 0; //not needed..its purpose is to avoid warning. Or:
-			  //void newP = maMalloc(size);
-			  //memcpy(newP, ptr, size);
-			  //myFree(ptr);
-			  //return newP;
+	void *newPtr = myMalloc(size);
+
+	Block *block = mem_block;
+	//shrink
+	if(block->meta.size > size)
+	{
+		size_t freed_space = block->meta.size + freed_space;
+		fillMeta(&block->meta, size, block->meta.next, block->meta.prev, 0);
+		
+		Block *freedBlock = (Block*) block->data - (block->meta.size - size);
+		myFree(freedBlock);
+		fillMeta(&block->meta, freed_space + sizeof(Meta), block->meta.next, block, 1);
+
+		return ptr;
+	}
+	//expand or malloc
+	if(block->meta.size < size)
+	{
+		memcpy(newPtr, ptr, size);
+		myFree(ptr);
+		return newPtr;
+	}
+	return NULL;
+
 }
 
-int main() {
-    init();
 
-    printf("Creating new dynamic memory of size 10.\n");
-    char *p = (char*)myMalloc(sizeof(char) * 10);
+void fillMeta(Meta *meta, size_t size, Block *next, Block *prev, unsigned char allocated) {
+	meta->allocated = allocated;
+	meta->size = size;
+	meta->next = next;
+	meta->prev = prev;
+}
 
-	for (int i = 0; i < 10; i++)
-		p[i] = i + '0';
+void merge(Block *block) {
+	if (block->meta.next) {
+		Block *next = block->meta.next;
+		if (!next->meta.allocated) {
+			block->meta.next = next->meta.next;
+			block->meta.size = next->meta.size + sizeof(Meta);
+			if (next->meta.next) {
+				next = next->meta.next;
+				next->meta.prev = block;
+			}
 
-	for(int i = 0; i < 10; i++)
-		printf("%c\n", p[i]);
+		}
+	}
+	if (block->meta.prev) {
+		Block *prev = block->meta.prev;
+		if (!prev->meta.allocated) {
+			prev->meta.next = block->meta.next;
+			prev->meta.size = block->meta.size + sizeof(Meta);
+			if (block->meta.next) {
+				block = block->meta.next;
+				block->meta.prev = prev;
+			}
+		}
+	}
+}
 
-	printf("\nFreeing the memory.\n");
-    myFree(&p);
-    if (!p)
-		printf("The memory is not free.\n");
-       
-    else 
-        printf("The memory is free.\n");
-       
-    return 0;
+
+
+int main () 
+{
+
+	init();
+	char *c1 = myMalloc(5 * sizeof(char));
+	char *c2 = myMalloc(10 * sizeof(char));
+	char *c3 = myMalloc(5 * sizeof(char));
+	char *c4 = myMalloc(10 * sizeof(char));
+	char *c5 = myMalloc(5 * sizeof(char));
+	strcpy(c1, "1111");
+	strcpy(c2, "222222222");
+	strcpy(c3, "3333");
+	strcpy(c4, "444444444");
+	strcpy(c5, "5555");
+	myFree(c2);
+	myFree(c4);
+	char *c6 = myMalloc(15 * sizeof(char)); //cant because no space availabe (
+	myFree(c3);
+	c6 = myMalloc(15 * sizeof(char)); // can because free blocks merging together when c3       released
+
+	return 0;
 }
